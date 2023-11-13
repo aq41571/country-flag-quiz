f51886686dcc5fedbe7fc1889bc5be58b045be91
aq41571 <genki.nakamura5@gmail.com>
2023-11-13 09:42:27 +0900

    feat: カラーを追記

--- src/app/ThemeRegistry/ThemeRegistry.tsx
+++ src/app/ThemeRegistry/ThemeRegistry.tsx
@@ -5,12 +5,17 @@ import { ThemeProvider } from '@mui/material/styles'
 
 import { createTheme } from '@mui/material'
 
-import { blue } from '@mui/material/colors'
+import { blue, grey } from '@mui/material/colors'
 import { NextAppDirEmotionCacheProvider } from './EmotionCache'
 import { roboto } from './font'
 
 const theme = createTheme({
-  palette: { mode: 'dark' },
+  palette: {
+    mode: 'dark',
+    background: { default: grey[900], paper: grey[800] },
+    primary: { main: blue[500] },
+    secondary: { main: blue.A400 },
+  },
   typography: { fontFamily: roboto.style.fontFamily },
   components: {
     MuiAlert: {
