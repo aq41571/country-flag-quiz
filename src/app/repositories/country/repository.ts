6fac80e599118b160e0798ef4a9190b99962a75e
aq41571 <genki.nakamura5@gmail.com>
2023-12-09 14:45:13 +0900

    fix: useCountriesRepository利用時にロード中の表示がされるように修正した

--- src/app/repositories/country/repository.ts
+++ src/app/repositories/country/repository.ts
@@ -1,3 +1,4 @@
+import { useSetFetchState } from '@/app/hooks'
 import { CountriesQuery, CountriesQueryVariables, useCountriesQuery } from '@/generated/graphql'
 import { QueryHookOptions } from '@apollo/client'
 import { useMemo } from 'react'
@@ -8,5 +9,6 @@ type CountriesOptions = QueryHookOptions<CountriesQuery, CountriesQueryVariables
 export const useCountriesRepository = (options?: CountriesOptions) => {
   const countriesQuery = useCountriesQuery(options)
   const countries = useMemo(() => convertCountries(countriesQuery.data), [countriesQuery.data])
+  useSetFetchState(countriesQuery.loading, countriesQuery.error)
   return { ...countriesQuery, countries }
 }
